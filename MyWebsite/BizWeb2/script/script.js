48fa65fb576e6545bd8d1e5b5e9f89163435bfe6
mgugan2 <mgmalak40@gmail.com>
2022-02-10 20:57:59 -0500

    CSS style for mobile 13

--- MyWebsite/BizWeb2/script/script.js
+++ MyWebsite/BizWeb2/script/script.js
@@ -1,6 +1,12 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
 
+var repl = /\wbox/;
+var filename="fbox";
+var catgmenu;
+var boxitem;
+var mysrcpath;
+
 function navToggle() {
   btn.classList.toggle('open')
   nav.classList.toggle('hidden')
@@ -8,3 +14,33 @@ function navToggle() {
 }
 
 btn.addEventListener('click', navToggle)
+
+function chgsrc() {
+  boxitem = document.querySelectorAll(".grid-col-2");
+  catgmenu = document.getElementById("category").value;
+  console.log("entered chgsrc", catgmenu);
+  getcatg();
+  if(boxitem != null) {
+    for (var i = boxitem.length; i--;) {
+      mysrcpath = boxitem[i].querySelector("img").src;
+      mysrcpath = mysrcpath.replace(repl,filename);
+      boxitem[i].querySelector("img").setAttribute("src", mysrcpath);
+    }
+  }
+  else
+    console.log("chgsrc boxitem is null");
+}
+
+function getcatg() {
+  console.log("entered getcatg");
+  if(catgmenu != null) {
+    if(catgmenu == "faucet") {
+      filename = "fbox";
+    }
+    else
+    { filename = "pbox";
+    }
+  }
+  else
+    console.log("getcatg doc id is null"); 
+}
